be27767144f48ebfb97254dd3a766bb32a999424
santi-nue <68065589+santi-nue@users.noreply.github.com>
2025-05-29 23:30:03 +0200

    Update local.mk

--- local.mk
+++ local.mk
@@ -1,5 +1,9 @@
-ifeq ($(CONFIG_PC),y) 
- TCPATH :=i686-w64-mingw32-
- CPU    :=i586
- MCPU   := -mcpu=$(CPU)
-endif
+
+HOST_IS_WIN32 := y
+HAVE_WIN32 := y
+HAVE_CE := n
+
+TCPATH :=i686-w64-mingw32-
+CPU    :=i586
+MCPU   := -mcpu=$(CPU)
+
